861c4972fe75db6e1260059e0195022c48c4ea44
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-05-22 18:06:50 -0400

    add separate pages for form and results

--- app.py
+++ app.py
@@ -13,13 +13,23 @@ def hello():
     if request.method == 'POST':
         print(request.form.get('name'))
         data = {'message': request.form.get('name')}
-        return render_template(
-            'hello.html',
-            data=data)
+        return render_template('hello.html', data=data)
     if request.method == 'GET':
-        return render_template(
-            'hello.html',
-            data=None)
+        return render_template('hello.html', data=None)
+
+@app.route("/hello_form")
+def hello_from():
+    return render_template('hello_form.html', data=None)
+
+@app.route("/hello_result", methods=['POST'])
+def hello_result():
+    #model = .....
+    #employee_age = request.form.get('age')
+    #employee_years_with_company = request.form.get('years_with_company')
+    #input = [employee_age, employee_years_with_company]
+    #result = model.predict(input)
+    data = {'message': request.form.get('name') }
+    return render_template('hello_result.html', data=data)
 
 if __name__ == "__main__":
     app.run()
